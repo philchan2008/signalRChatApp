6c28c4c39257a2257ccd875e686a5f75da8f00dd
phil <phil@flexsystem.com>
2025-09-27 19:01:59 +0100

    + add pagination in history display page

--- Pages/History.cshtml.cs
+++ Pages/History.cshtml.cs
@@ -18,11 +18,21 @@ namespace SignalRChatApp.Pages
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int p = 1)
         {
+            const int pageSize = 10;
+            int page = p;
+            CurrentPage = page;
+
+            var totalMessages = await _context.Messages.CountAsync();
+            TotalPages = (int)Math.Ceiling(totalMessages / (double)pageSize);
+
             Messages = await _context.Messages
                 .OrderByDescending(m => m.Timestamp)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
                 .ToListAsync();
         }
+
     }
 }
